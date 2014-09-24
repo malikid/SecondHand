19a7e0a0f9c56f8805ab725451d714c61830ce34
malikid <malikid.chang@gmail.com>
2014-09-25 01:50:06 +0800

    Added bower.

---
+++ tasks/config/bower.js
@@ -0,0 +1,24 @@
+module.exports = function(grunt) {
+  grunt.config.set("bower", {
+    dev: {
+      options: {
+        layout: "byComponent",
+        targetDir: './assets/bower',
+        install: true,
+        cleanTargetDir: true
+      }
+    },
+    prod: {
+      options: {
+        layout: "byComponent",
+        targetDir: './assets/bower',
+        install: true,
+        cleanTargetDir: true,
+        cleanBowerDir: true
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-bower-task');
+  return;
+}
